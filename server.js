3d15fdc1cb084d42c5488659a344b0e0c10e0b2c
AadeshMndr <125436995+AadeshMndr@users.noreply.github.com>
2023-04-19 17:54:00 +0545

    6

--- server.js
+++ server.js
@@ -1,11 +1,7 @@
-const http = require("http");
-
 const app = require("./app");
 
 require("dotenv").config();
 
 const port = 3000;
 
-const server = http.createServer(app);
-
-server.listen(port);
+app.listen(port);
